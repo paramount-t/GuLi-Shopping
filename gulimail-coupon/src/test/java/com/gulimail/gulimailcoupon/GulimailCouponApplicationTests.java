8df497fb3a5b19cc7bfdb9deba8fa08c9fcec854
local <local@qq.com>
2021-10-26 17:10:08 +0800

    the first time

---
+++ gulimail-coupon/src/test/java/com/gulimail/gulimailcoupon/GulimailCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gulimail.gulimailcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimail-ware/src/test/java/com/gulimail/guliware/GuliWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gulimail.guliware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
